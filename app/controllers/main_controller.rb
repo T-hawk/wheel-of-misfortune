a3746329bffeea50fe8e3d80cb58a7b3e335616f
Thomas Grinstead <tomahawkga@gmail.com>
2019-12-16 17:09:22 -0500

    Added highscore functionality

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,24 @@
+include MainHelper
+
 class MainController < ApplicationController
   def index
   end
 
   def scoreboard
+    @games = FinishedGame.all
+    @victorious_teams = []
+
+    @games.each do |game|
+      teams = eval game.teams
+      @victorious_teams << {victorious_team: teams[game.victorious_team], date: game.updated_at}
+    end
+
+    limit = 50
+    @ordered_teams = MainHelper.sort_teams(@victorious_teams)
+
+    if @ordered_teams.length > limit
+      @ordered_teams.slice!(limit, @ordered_teams.length - limit)
+    end
+
   end
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,26 @@
 module MainHelper
+
+  def sort_teams(teams)
+    if teams.length < 2
+      return teams
+    else
+      pivot = teams[rand(teams.length)]
+
+      less = []
+      greater = []
+      (teams - [pivot]).each do |team| 
+        if team[:victorious_team]["score"] > pivot[:victorious_team]["score"]
+          greater << team
+        else
+          less << team
+        end
+      end
+
+      return sort_teams(greater) + [pivot] + sort_teams(less)
+    end
+  end
+
+  def convert_date(date)
+    return "#{Date::MONTHNAMES[date.month.to_i]} #{date.day}, #{date.year}"
+  end
 end
